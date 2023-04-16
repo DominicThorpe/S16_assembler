a6b833fbfd863f7e4873f5b4a600eb71489bae5b
thorped1 <dompthorpe123@gmail.com>
2023-04-16 17:04:29 +0100

    Fixed lable table generation

--- src/label_table.rs
+++ src/label_table.rs
@@ -1,12 +1,75 @@
+use std::collections::HashMap;
+use std::io::{BufRead, BufReader};
+use std::fs::File;
+
 use crate::validation::validate_label;
 
-pub fn get_label_table_entry_from_line(line_num:usize, line:&str) -> Option<(String, usize)> {
-    match line.find(":") {
-        Some(index) => {
-            validate_label(&line[..index]).unwrap();
-            Some((line[..index].to_string(), line_num))
-        },
 
-        None => None
+/**
+ * Takes a filename as input and generates the label table for that file where the label is the key and the 
+ * address of the label is the value.
+ */
+pub fn get_label_table(input_file:&File) -> HashMap<String, usize> {
+    let mut lable_table:HashMap<String, usize> = HashMap::new();
+
+    let mut line_num:usize = 0;
+
+    // filter out all empty lines and trim away whitespace
+    let input_lines:Vec<String> = BufReader::new(input_file).lines().filter_map(|line| match line.unwrap().trim() {
+        "" => None, 
+        l => Some(l.to_string())
+    }).collect();
+
+    for line in input_lines {
+        if line.ends_with(":") { // if the line is just a label
+            let label = line[..line.len() - 1].to_string();
+
+            validate_label(&label).unwrap();
+            lable_table.insert(label, line_num);
+
+            continue;
+        } else if let Some(index) = line.find(":") { // if the line is a label and an instruction
+            let label = line[..index].to_string();
+
+            line[..line.len() - 1].to_string();
+            lable_table.insert(label, line_num);
+        }
+
+        // add 2 lines for a 16 bit instr and 4 for a 32 bit instr
+        match line.to_lowercase().contains("movi") {
+            true => line_num += 4,
+            false => line_num += 2
+        }
+    }
+
+    lable_table
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use std::fs::OpenOptions;
+
+    use super::get_label_table;
+
+
+    #[test]
+    fn test_label_table_generation() {
+        let input_file = OpenOptions::new().read(true).open("test_files/test_label_table_gen.asm").unwrap();
+        let label_table = get_label_table(&input_file);
+
+        assert_eq!(label_table["start"], 0);
+        assert_eq!(label_table["label_2"], 4);
+        assert_eq!(label_table["label_3"], 6);
+        assert_eq!(label_table["label_4"], 12);
+    }
+
+
+    #[test]
+    #[should_panic]
+    fn test_invalid_label() {
+        let input_file = OpenOptions::new().read(true).open("test_files/test_invalid_label.asm").unwrap();
+        let _ = get_label_table(&input_file);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ mod validation;
 mod label_table;
 
 use assembler::process_line;
-use label_table::get_label_table_entry_from_line;
+use label_table::get_label_table;
 
 
 
@@ -19,12 +19,7 @@ fn main() {
 
     let input_file = OpenOptions::new().read(true).open(filename).unwrap();
 
-    let label_table:HashMap<String, usize> = BufReader::new(&input_file).lines()
-                                        .enumerate()
-                                        .filter_map(|(line_num, line)| match get_label_table_entry_from_line(line_num, &line.unwrap()) {
-                                            Some(label) => Some(label),
-                                            None => None
-                                        }).collect();
+    let label_table:HashMap<String, usize> = get_label_table(&input_file);
     let input_lines = BufReader::new(&input_file).lines().filter_map(|line| match line.unwrap().trim() {
         "" => None, 
         l => process_line(l, &label_table)
